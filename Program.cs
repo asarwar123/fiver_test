0ae554bc28dbcf3048bbb186a2f94b92a2755a8e
Azeem Sarwar <asarwar.kpsl@gmail.com>
2022-05-10 22:55:06 +0500

    first

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+string ExecutableFilePath = @"Scripts.bat";
+string Arguments = @"";
+
+if (File.Exists(ExecutableFilePath))
+{
+    ProcessStartInfo psi = new ProcessStartInfo(ExecutableFilePath, Arguments);
+    psi.UseShellExecute = false;
+    psi.CreateNoWindow = true;
+    Process.Start(psi);
+}
+
+Console.WriteLine("Hello, World!");
---
+++ bin/Debug/net6.0/Scripts.bat
@@ -0,0 +1,1 @@
+start cmd /k "nmap -p 9100,515,631 192.168.1.1/24 -oX printers.xml"
